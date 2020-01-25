1508118af74256aacac44093f63455cb3e6009e0
xayron <zxyron0@gmail.com>
2020-01-25 11:07:09 +0530

    Add Navigation Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,41 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                'Walls',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              title: Text(
+                'Item 1',
+              ),
+            ),
+                        ListTile(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              title: Text(
+                'Item 1',
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
